170b3c3f72644fe38bba37b9ff2055b0f667e6f0
Daniel Garcia <jdaniel_gar@hotmail.com>
2017-10-25 16:46:02 -0500

    Final Linear Regression Gradient

---
+++ Flowers/flowers.py
@@ -0,0 +1,15 @@
+#%%
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+#%%
+df = pd.read_csv("data2.csv")
+data = df.as_matrix()
+
+#%%
+
+# Convert the string data
+
+D = 4;
+N = 100;
+
