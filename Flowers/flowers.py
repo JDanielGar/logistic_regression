e0118aefd67bf6a9c53d6cd555d37a34cc0ce962
unknown <daniel.garcia@banlinea.com>
2017-10-26 00:43:34 -0500

    Doing the Petal Project

--- Flowers/flowers.py
+++ Flowers/flowers.py
@@ -8,8 +8,33 @@ data = df.as_matrix()
 
 #%%
 
-# Convert the string data
-
 D = 4;
 N = 100;
 
+
+data[data == 'Iris-setosa'] = 0;
+data[data == 'Iris-versicolor'] = 1;
+
+X = data[:, :-1].astype(float)
+
+Y = data[:, -1:].astype(float)
+W = np.random.randn(D, 1);
+
+#%%
+
+def sigmoid(X):
+    return 1 / (1 + np.exp(-X))
+
+#%%
+    
+def forward(X, W):
+    z = sigmoid(X.dot(W));
+    print(z)
+    return z;
+
+
+#%%
+    
+# Probe ur self.
+    
+forward(X, W)
