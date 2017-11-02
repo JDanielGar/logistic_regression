30db2ab8ccc160d7502efd69da36da663d094d05
unknown <daniel.garcia@banlinea.com>
2017-11-02 04:53:12 -0500

    Model console program initial structure

---
+++ ClotheModel/current_session.py
@@ -0,0 +1,114 @@
+# coding: utf-8
+
+import matplotlib as plt
+import numpy as np
+import pandas as pd
+    
+data = pd.read_excel('data.xlsx')
+data_likes = data.as_matrix()
+
+# data[left:right, bottom:top] data[delete columns, delete rows]
+
+# Samples and Dimension
+
+N, D = data_likes.shape
+
+# To Text
+
+data_likes[:, 1][data_likes[:, 1] == 0] = 'Sin mangas'
+data_likes[:, 1][data_likes[:, 1] == 1] = 'Corta'
+data_likes[:, 1][data_likes[:, 1] == 2] = 'Larga'
+
+# get_ipython().magic('save previous_session ~1/') # 1 = Ipython
+# get_ipython().magic('save current_session ~0/') # 2 = Python
+
+data_likes[:, 2][data_likes[:, 2] == 0] = 'Polo'
+data_likes[:, 2][data_likes[:, 2] == 1] = 'Scoop'
+data_likes[:, 2][data_likes[:, 2] == 2] = 'V-Neck'
+
+
+# Here come the LIKES
+
+likes = np.zeros([D, 1])
+# likes = [1, 1, 0, 1, 0, 0, 0, 0, 0]
+
+# likes = []
+
+def getLikes():
+    like = [];
+    for samples in range(0, N):
+        print("Nombre de prenda", data_likes[samples, 0], "\n Caracteristicas: \n  1)Manga:", data_likes[samples, 1])
+        print("  2)Cuello:", data_likes[samples, 2], "\n  3)Bolsillos:", data_likes[samples, 3], "\n  4)Botones:", data_likes[samples, 4])
+        print("  5)Imagen:", data_likes[samples, 5])
+        likes[samples, :] = int(input("¿Te gusta?. Si(1) | No(2) -> "))
+
+# Here Come the predictions
+
+data_matrix = data.as_matrix()[:, 1:]
+
+# 1) Normalize Columns
+
+data_matrix[:, 0] = (data_matrix[:, 0] - data_matrix[:, 0].mean()) / data_matrix[:, 0].std()
+data_matrix[:, 1] = (data_matrix[:, 1] - data_matrix[:, 1].mean()) / data_matrix[:, 1].std()
+
+# 2) Stardard Varibles
+
+X = data_matrix
+Y = likes
+W = np.random.rand(D-1, 1)
+b = 0
+
+# 3) Define Train and Test
+
+Xtrain = X[:6, :]
+Ytrain = Y[:6]
+
+Xtest = X[-3:, :]
+Ytest = X[-3]
+
+# 4) Define forward functions
+
+def sigmoid(x): # Sigmoid function to give a importance to the data
+    return 1/(1+np.exp(-x))
+    
+def forward(w, x, b): # Do the prediction
+    return sigmoid(x.dot(w)) + b
+
+# 5) Define predict function and costs
+
+train_costs = []
+test_costs = []
+
+def predict_weight(w, x, b, learn_rate = 0.01, iterations=1000):
+    for i in range(iterations):
+        pYtrain = forward(w, Xtrain, b)
+        pYtest = forward(w, Ytest, b)
+
+        ctrain = cross_entropy(Ytrain, pYtrain)
+        ctest = cross_entropy(Ytest, pYtest)
+
+        train_costs.append(ctrain)
+        test_costs.append(ctest)
+
+        gradientDescent(Xtrain, Ytrain, pYtrain, learn_rate);
+
+        if(1 % 1000 == 0):
+            print(i, ctrain, ctest)
+
+    print("Final train Classification rate", classification_rate(Ytrain, np.round(pYtrain)))
+    print("Final test classification rate", classification_rate(Ytest, np.round(pYtest)))
+
+
+# 6) Classification for seeing the results
+
+def classification_rate(Y, P):
+    return np.mean(Y == P)
+
+
+def gradientDescent(Xtrain, Ytrain, pYtran, learn_rate):
+    W -= learning_rate * Xtrain.T.dot(pYtrain - Ytrain)
+    b -= learning_rate * (pYtrain - Ytrain).sum()
+    
+
+def cross_entropy(t, y): # Targets and predictions
+    return t * np.log(y) + (1-t) * np.log(1-y)
---
+++ ClotheModel/other.py
@@ -0,0 +1,81 @@
+# coding: utf-8
+import pandas as pd
+import numpy as np
+from sklearn.utils import shuffle
+df = pd.read_csv('dog.csv')
+data = df.as_matrix()
+X = data[:, :-1].astype(float)
+Y = data[:, -1:]
+
+# Normalize the data
+X[:, 0] = (X[:, 0] - X[:, 0].mean()) / X[:, 0].std()
+X[:, 1] = (X[:, 1] - X[:, 1].mean()) / X[:, 1].std()
+X[:, 2] = (X[:, 2] - X[:, 2].mean()) / X[:, 2].std()
+
+# Dimensions of Array
+
+N, D = X.shape
+
+X, Y = shuffle(X, Y)
+
+# Initialize Weights and Bias Term.
+
+W = np.random.rand(D, 1)
+b = 0
+def sigmoid(X):
+    return 1 / (1 + np.exp(-X))
+
+
+def forward(X, W, b):
+    return sigmoid(X.dot(W) + b)
+Xtrain = X[:6, :]
+Ytrain = Y[:6, :]
+
+Xtest = X[-3:]
+Ytest = Y[-3:]
+def classification_rate(Y, P):
+    return np.mean(Y == P)
+def cross_entropy(T, pY):
+    return -np.mean(T*np.log(pY) + (1 - T)*np.log(1 - pY))
+train_costs = []
+test_costs = []
+learning_rate = .001
+for i in range(10000):
+    pYtrain = forward(Xtrain, W, b)
+    pYtest = forward(Xtest, W, b)
+    
+    ctrain = cross_entropy(Ytrain, pYtrain)
+    ctest = cross_entropy(Ytest, pYtest)
+    
+    train_costs.append(ctrain)
+    test_costs.append(ctest)
+    
+    # Gradient Descent
+    
+    W -= learning_rate * Xtrain.T.dot(pYtrain - Ytrain)
+    b -= learning_rate * (pYtrain - Ytrain).sum()
+    
+    if(1 % 1000 == 0):
+        print(i, ctrain, ctest)
+
+
+print("Final train Classification rate", classification_rate(Ytrain, np.round(pYtrain)))
+print("Final test classification rate", classification_rate(Ytest, np.round(pYtest)))
+def predict(X, W, b):
+    print(forward(X, W, b))
+
+
+test = np.array([3, 2, 1, 1])
+predict(test, W, b)
+def predict(X, W, b):
+    print(forward(X, W, b))
+
+
+test = np.array([0, 0, 1, 1])
+predict(test, W, b)
+def predict(X, W, b):
+    print(forward(X, W, b))
+
+
+test = np.array([0, 0, 1, 0])
+predict(test, W, b)
